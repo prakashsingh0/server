83d2a0531083a5b81c548b31faa684512de6c17d
ASKshatreeya06 <singhprakash545@gmail.com>
2025-03-12 00:35:11 +0530

    updates

--- app.js
+++ app.js
@@ -39,5 +39,5 @@ app.use((req, res, next) => {
   res.status(404).json({ error: "API route not found" });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
